51599d3da5bb1700564bb74d470146f74629e876
thisemersonaraujo <emerson.dev@outlook.com>
2022-08-24 22:55:36 +0100

    React Bootstrap + Login btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { Button } from 'react-bootstrap';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="p-1">
+      <Button variant="primary" className="mr-1">Login</Button>
+    </div>
   )
 }
 
